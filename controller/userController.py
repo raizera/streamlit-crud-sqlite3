fb73fa6f645cc6f0e08a0f78511073164955148a
raizera <raitorresx@gmail.com>
2022-12-20 14:25:04 -0300

    commit - final update

--- controller/userController.py
+++ controller/userController.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+import streamlit as st
+
 import models.cadastro as cadastro
 
-conn = sqlite3.connect('data.db')
+conn = sqlite3.connect('data.db', check_same_thread=False)
 c = conn.cursor()
 
 
@@ -11,42 +13,88 @@ c = conn.cursor()
 
 def create_table():
     c.execute(
-        'CREATE TABLE IF NOT EXISTS cadastro (nome TEXT, email TEXT, profissao TEXT, sexo TEXT, idade INTEGER, '
-        'estado TEXT)')
+        'CREATE TABLE IF NOT EXISTS cadastro(nome VARCHAR(20) NOT NULL, email VARCHAR(15) NOT NULL, profissao VARCHAR(15) NOT NULL, sexo CHAR(21) NOT NULL, idade INTEGER NOT NULL, estado CHAR(2) NOT NULL)')
 
-#----------CREATE----------#
+# ----------CREATE----------#
 
 
-def create(nome, email, profissao, sexo, idade, estado):
-    c.execute('INSERT INTO cadastro (nome, email, profissao, sexo, idade, estado) VALUES (?, ?, ?, ?, ? ,?)',
-              (nome, email, profissao, sexo, idade, estado)
-              )
+def add_data(nome, email, profissao, sexo, idade, estado):
+    c.execute('INSERT INTO cadastro (nome, email, profissao, sexo, idade, estado) VALUES (?, ?, ?, ?, ?, ?)',
+              (nome, email, profissao, sexo, idade, estado))
     conn.commit()
-#----------end CREATE----------#
+# ----------end CREATE----------#
 
-#-------------READ-------------#
-
-
-def read_():
-    c.execute('SELECT * FROM cadastro')
-    data = c.fetchall()
-    return data
+# -------------READ-------------#
 
 
 def read():
     c.execute('SELECT * FROM cadastro')
-    read_list = []
+    data = c.fetchall()
+    return data
+# ----------end READ----------#
+
+
+# -----------UPDATE-----------#
+
+def update(novo_nome, novo_email, nova_profissao, novo_sexo, nova_idade, novo_estado, nome, email, profissao, sexo, idade, estado):
+    c.execute(
+        "UPDATE cadastro SET nome=?, email=?, profissao=?, sexo=?, idade=?, estado=? WHERE nome=? and email=? and profissao=? and sexo=? and idade=? and estado=?", (novo_nome, novo_email, nova_profissao, novo_sexo, nova_idade, novo_estado, nome, email, profissao, sexo, idade, estado))
+    conn.commit()
+    data = c.fetchall()
+    return data
+
+
+# ---------end UPDATE---------#
+
+# ---------DELETE---------#
+
+
+def delete(nome):
+    c.execute('DELETE FROM cadastro WHERE nome = "{}"'.format(nome))
+    conn.commit()
+
+    # ---------end DELETE---------#
+
+
+def get_task(nome):
+    c.execute('SELECT * FROM cadastro WHERE nome = "{}"'.format(nome))
+    data = c.fetchall()
+    return data
+
+
+def selecionarTodos():
+    c.execute("SELECT * FROM cadastro")
+    costumerList = []
 
     for row in c.fetchall():
-        read_list.append(cadastro.Cadastro(
+        costumerList.append(cadastro.Cadastro(
             row[0], row[1], row[2], row[3], row[4], row[5]))
-    return read_list
-#----------end READ----------#
+    return costumerList
 
 
-#-----------UPDATE-----------#
+def List():
+    colms = st.columns((1, 1, 1, 1, 1, 1, 1))
+    campos = ['nome', 'email', 'profissão',
+              'sexo', 'idade', 'estado', 'ação']
 
-def update():
-    c.execute('')
+    for col, campo in zip(colms, campos):
+        col.write(campo)
 
-#---------end UPDATE---------#
+    for item in selecionarTodos():
+        col1, col2, col3, col4, col5, col6, col7 = st.columns(
+            (1, 1, 1, 1, 1, 1, 1))
+
+        col1.write(item.nome)
+        col2.write(item.email)
+        col3.write(item.profissao)
+        col4.write(item.sexo)
+        col5.write(item.idade)
+        col6.write(item.estado)
+
+        button_space = col7.empty()
+
+        on_click = button_space.button(
+            'alterar', 'btnAlterar' + str(item.nome))
+
+        if on_click:
+            st.write("botao pressionado")
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pandas as pd
+
 import streamlit as st
 
 import controller.userController as usrc
@@ -7,62 +7,136 @@ import controller.userController as usrc
 def main():
     st.title("CRUD with Streamlit")
 
-    menu = ["Create", "Read", "Update", "Delete", "About"]
+    menu = ["Home", "Cadastrar", "Consultar", "Alterar", "Deletar"]
     choice = st.sidebar.selectbox("Menu", menu)
 
-    if choice == "Create":
+    if choice == "Home":
+        st.subheader("Bem-vindo ao sistema de cadastro no Streamlit!")
+        st.subheader("Navegue pelo menu lateral.")
+
+    elif choice == "Cadastrar":
         st.subheader("Adicionar usuário")
 
         col1, col2 = st.columns(2)
 
         with col1:
-            nome = st.text_input(label="nome")
+            nome = st.text_input(label="NOME")
             sexo = st.selectbox(
-                "sexo", ['Masculino', 'Feminino', 'Prefiro não responder'])
-            profissao = st.text_input(label="profissão")
+                "SEXO", ['', 'Masculino', 'Feminino', 'Prefiro não responder'], format_func=lambda x: 'Selecione o sexo' if x == '' else x)
+            profissao = st.text_input(label="PROFISSÃO")
 
         with col2:
-            email = st.text_input(label="e-mail")
-            idade = st.number_input("idade", format="%d", step=1)
-            estado = st.selectbox("estado",
-                                  ['AC', 'AL', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB',
-                                   'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'])
+            email = st.text_input(label="E-mail")
+            idade = st.date_input(
+                "DATA DE NASCIMENTO (AAAA/MM/DD)", min_value=None)
+            estado = st.selectbox("ESTADO",
+                                  ['', 'AC', 'AL', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB',
+                                   'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'], format_func=lambda x: 'Selecionar estado' if x == '' else x)
 
         if st.button("cadastrar"):
-            usrc.create_table()
-            usrc.create(
-                nome, email, profissao, sexo, idade, estado
-            )  # essa ordem é importante estar correta
-            st.success("cadastrado com sucesso!")
+            if nome == '':
+                st.warning('Você esqueceu de preencher o nome.', icon="⚠️")
+            elif sexo == '':
+                st.warning('Você esqueceu de preencher o sexo.', icon="⚠️")
+            elif profissao == '':
+                st.warning(
+                    'Você esqueceu de preencher a profissão.', icon="⚠️")
+            elif email == '':
+                st.warning('Você esqueceu de preencher o e-mail.', icon="⚠️")
+            elif idade == 0:
+                st.warning('Você esqueceu de preencher a idade.', icon="⚠️")
+            elif estado == '':
+                st.warning('Você esqueceu de preencher o estado.', icon="⚠️")
+            else:
+                usrc.create_table()
+                usrc.add_data(
+                    nome, email, profissao, sexo, idade, estado
+                )  # essa ordem é importante estar correta
+                st.success("cadastrado com sucesso!")
 
-    elif choice == "Read":
+    elif choice == "Consultar":
         st.subheader("Consultar usuário")
-        tabela_ = usrc.read_()
-        st.table(df)
-        # st.write(tabela_)
+        with st.expander("ver tabela de cadastrados"):
+            tabela = usrc.read()
+            st.dataframe(tabela)
 
-    elif choice == "Update":
+        with st.expander("ver cadastrados (JSON)"):
+            st.write(tabela)  # dados json
+
+    elif choice == "Alterar":
 
         st.subheader("Atualizar usuário")
 
-    elif choice == "Delete":
+        nome = [i[0] for i in usrc.read()]
+        alterar = st.selectbox("Nome", nome)
+        resultado = usrc.get_task(alterar)
+        st.write(resultado)
+
+        if resultado:
+            nome = resultado[0][0]
+            email = resultado[0][1]
+            profissao = resultado[0][2]
+            sexo = resultado[0][3]
+            idade = resultado[0][4]
+            estado = resultado[0][5]
+
+            #tabela = usrc.read()
+
+            col1, col2 = st.columns(2)
+
+            with col1:
+                novo_nome = st.text_input('NOME', nome)
+                nova_profissao = st.text_input('PROFISSÃO', profissao)
+                novo_sexo = st.selectbox('SEXO', ['{}'.format(sexo), 'Masculino', 'Feminino', 'Prefiro não responder'],
+                                         format_func=lambda x: 'Selecione o sexo' if x == '' else x)
+            with col2:
+                novo_email = st.text_input('E-mail', email)
+                nova_idade = st.date_input(
+                    label='IDADE')
+                novo_estado = st.selectbox('ESTADO',
+                                           ['{}'.format(estado), 'AC', 'AL', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB',
+                                            'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'], format_func=lambda x: 'Selecionar estado' if x == '' else x)
+
+                # usrc.update(nome, email, profissao, sexo, idade, estado) #esta linha está gerando erro...
+
+            if st.button("Alterar"):
+                if novo_nome == '':
+                    st.warning('Você esqueceu de preencher o nome.', icon="⚠️")
+                elif novo_sexo == '':
+                    st.warning('Você esqueceu de preencher o sexo.', icon="⚠️")
+                elif nova_profissao == '':
+                    st.warning(
+                        'Você esqueceu de preencher a profissão.', icon="⚠️")
+                elif novo_email == '':
+                    st.warning(
+                        'Você esqueceu de preencher o e-mail.', icon="⚠️")
+                elif nova_idade == 0:
+                    st.warning(
+                        'Você esqueceu de preencher a idade.', icon="⚠️")
+                elif novo_estado == '':
+                    st.warning(
+                        'Você esqueceu de preencher o estado.', icon="⚠️")
+                else:
+                    # essa ordem é importante estar correta
+                    usrc.update(novo_nome, novo_email, nova_profissao,
+                                novo_sexo, nova_idade, novo_estado, nome, email, profissao, sexo, idade, estado)
+                    st.success("alterado com sucesso!")
+
+        # usrc.List()
+
+    elif choice == "Deletar":
         st.subheader("Deletar usuário")
 
+        nome = [i[0] for i in usrc.read()]
+        delete_nome = st.selectbox("Nome", nome)
+
+        if st.button("Delete"):
+            usrc.delete(delete_nome)
+            st.warning("{} foi deleteado!".format(delete_nome))
+
     else:
-        st.subheader("Sobre")
+        st.subheader("Home")
 
-
-# gerando a lista com pandas
-read_list = []
-
-for item in usrc.read():
-    read_list.append([item.nome, item.email, item.profissao,
-                     item.sexo, item.idade, item.estado])
-
-df = pd.DataFrame(
-    read_list,
-    columns=['nome', 'email', 'profissao', 'sexo', 'idade', 'estado']
-)
 
 if __name__ == '__main__':
     main()
